8bc914866c3c7bbfade2058667311181dcc6eaae
evaaapradhan <evasanapradhan@gmail.com>
2022-03-14 20:51:30 +0545

    react UI completed

--- src/page/homepage/Home.jsx
+++ src/page/homepage/Home.jsx
@@ -1,15 +1,21 @@
+  
 import Headline from "../../components/headline/Headline"
 import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
+import Login from "../login/Login"
+import Settings from "../settings/Settings"
 import "./home.css"
 
 export default function Home() {
+  
   return (
     <>
-      <Headline />
+      {/* <Headline /> */}
       <div className="home">
-         <Posts />
-         <Sidebar />
+         {/* <Posts /> */}
+         {/* <Sidebar /> */}
+         {/* <Settings /> */}
+         <Login />
         
       </div>
     </>
